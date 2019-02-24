d7a5f6138a38b4436e71171e77b0dd562c8c091d
aaron-ecs <aaron.williams@ecs-digital.co.uk>
2019-02-24 15:48:10 +0000

    Merge of ec2_unit_test branch

--- deploy_to_ec2_test.py
+++ deploy_to_ec2_test.py
@@ -11,6 +11,10 @@ from botocore.exceptions import ClientError
 import deploy_to_ec2
 
 
+class MockClient(object):
+    pass
+
+
 class DeployToEC2Test(unittest.TestCase):
     """
     Test class for deploying to EC2.
@@ -36,11 +40,13 @@ class DeployToEC2Test(unittest.TestCase):
         # THEN
         assert 'Unable to open a connection to codedeploy' in output
 
+    @patch('deploy_to_ec2.open_connection', return_class=MockClient)
     @patch('deploy_to_ec2.deploy_artifact')
-    def test_start_deployment_error(self, mock_boto3):
+    def test_start_deployment_error(self, mock_boto3, mock_client):
         """ Test for exit code and logging if deployment does not start """
         # GIVEN
         sys.stdout = StringIO()
+        mock_client.return_class = MockClient()
         mock_boto3.side_effect = ClientError(
             {
                 "Error": {
@@ -56,12 +62,14 @@ class DeployToEC2Test(unittest.TestCase):
         # THEN
         assert 'Error when starting deployment' in output
 
+    @patch('deploy_to_ec2.open_connection', return_class=MockClient)
     @patch('deploy_to_ec2.deploy_artifact')
     @patch('deploy_to_ec2.get_deployment_status', return_value="Succeeded")
-    def test_deployment_succeeded(self, deploy_artifact, get_status):
+    def test_deployment_succeeded(self, deploy_artifact, get_status, mocked_client):
         """ Test for exit code and logging if deployment runs okay """
         # GIVEN
         sys.stdout = StringIO()
+        mocked_client.return_class = MockClient
         deploy_artifact.return_json = "Succeeded"
         get_status.return_value = "Succeeded"
 
@@ -73,12 +81,14 @@ class DeployToEC2Test(unittest.TestCase):
         # THEN
         assert 'Deployment is a success' in output
 
+    @patch('deploy_to_ec2.open_connection', return_class=MockClient)
     @patch('deploy_to_ec2.deploy_artifact')
     @patch('deploy_to_ec2.get_deployment_status', return_value="Failed")
-    def test_deployment_failed(self, deploy_artifact, get_status):
+    def test_deployment_failed(self, deploy_artifact, get_status, mock_client):
         """ Test for exit code and logging if deployment fails """
         # GIVEN
         sys.stdout = StringIO()
+        mock_client.return_class = MockClient
         deploy_artifact.return_json = "Failed"
         get_status.return_value = "Failed"
 
@@ -90,12 +100,14 @@ class DeployToEC2Test(unittest.TestCase):
         # THEN
         assert 'Deployment has failed!' in output
 
+    @patch('deploy_to_ec2.open_connection', return_class=MockClient)
     @patch('deploy_to_ec2.deploy_artifact')
     @patch('deploy_to_ec2.get_deployment_status', return_value="Stopped")
-    def test_deployment_stopped(self, deploy_artifact, get_status):
+    def test_deployment_stopped(self, deploy_artifact, get_status, mock_client):
         """ Test for exit code and logging if deployment is stopped """
         # GIVEN
         sys.stdout = StringIO()
+        mock_client.return_class = MockClient
         deploy_artifact.return_json = "Stopped"
         get_status.return_value = "Stopped"
 
@@ -106,3 +118,4 @@ class DeployToEC2Test(unittest.TestCase):
 
         # THEN
         assert 'Deployment has failed!' in output
+
